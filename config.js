fd5774b5384c86d2676001877adf1fa5c1d3daac
Vượng Nguyễn <119097170+vuongvip12@users.noreply.github.com>
2023-09-23 05:25:24 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,20 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
+    titleWeb: "Hêlu bn nha!!!(thoát ra là đồng ý lm ngeo mình nhé!!!)",
+    introTitle: 'Nguyễn Thị Hải Yến',
     introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    Mà sao cậu cứ đi nhầm đường
+    Lạc vào tim tớ vậy<3
+    Yêu cậu nhìu`,
     btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+    title: 'Làm Ngiu tớ khum 🥰',
+    desc: 'cậu cũng thích tớ đúng khum!!! ',
     btnYes: 'Thích lắm <33',
     btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
+    question: 'Tại sao cậu lại yêu tớ?',
     btnReply: 'Gửi cho bạn <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
+    messDesc: 'Vậy thì cùng set love khum.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
+    messLink: ' https://www.facebook.com/messages/t/vuongnguyen1006' //link mess của các bạn. VD:
 }
